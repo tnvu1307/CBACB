2fa5c2aecd28ac7b6e65822ed336cfc9243fb173
Trương Nhật Vy <vytruongjb@gmail.com>
2024-01-25 17:10:39 +0700

    fix: update version to fix GGLOGERR_82

--- AppCore/AssemblyInfo.vb
+++ AppCore/AssemblyInfo.vb
@@ -30,6 +30,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.0.1.5")>
+<Assembly: AssemblyVersion("4.0.1.6")>
 
-<Assembly: AssemblyFileVersion("4.0.1.5")>
+<Assembly: AssemblyFileVersion("4.0.1.6")>
