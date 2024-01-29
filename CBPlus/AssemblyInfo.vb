28645c762af1330cb6905254a61ea4cc2d6bae7e
Trương Nhật Vy <vytruongjb@gmail.com>
2024-01-29 09:45:55 +0700

    build: update version 4021

--- CBPlus/AssemblyInfo.vb
+++ CBPlus/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.0.2.0")>
+<Assembly: AssemblyVersion("4.0.2.1")>
 
-<Assembly: AssemblyFileVersion("4.0.2.0")>
+<Assembly: AssemblyFileVersion("4.0.2.1")>
