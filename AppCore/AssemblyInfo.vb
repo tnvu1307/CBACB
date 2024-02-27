4be1afea52f62a39ee6ee7f50ca00b606e434731
Trương Nhật Vy <vytruongjb@gmail.com>
2024-02-27 10:58:33 +0700

    build: update version 4034

--- AppCore/AssemblyInfo.vb
+++ AppCore/AssemblyInfo.vb
@@ -30,6 +30,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.0.3.3")>
+<Assembly: AssemblyVersion("4.0.3.4")>
 
-<Assembly: AssemblyFileVersion("4.0.3.3")>
+<Assembly: AssemblyFileVersion("4.0.3.4")>
